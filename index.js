984f31172bb858b3a6697573194d014aadc80032
negaonka <neha.n.gaonkar@gmail.com>
2022-09-15 19:02:46 +0100

    adding callback function

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-function callbackFunction(arr, func) {}
+function callbackFunction(arr, func) {
+  const result = arr.map((i) => {
+    return func(i);
+  });
+  return result;
+}
 
 module.exports = callbackFunction;
--- index.test.js
+++ index.test.js
@@ -12,24 +12,32 @@
 // Output: [-1, 0, 1]
 
 const callbackFunction = require("./index");
-describe("test case describing the callback functions", ()=>{
-    it("should return [2,4,6]", ()=>{
-        const result = callbackFunction([1,2,3], function(num){return num * 2});
-        expect(result).toEqual([2,4,6])
-    })
+describe("test case describing the callback functions", () => {
+  it("should return [2,4,6]", () => {
+    const result = callbackFunction([1, 2, 3], function (num) {
+      return num * 2;
+    });
+    expect(result).toEqual([2, 4, 6]);
+  });
 
-    it("should return [2,3,4]", ()=>{
-        const result = callbackFunction([1,2,3], function(num){return num + 1});
-        expect(result).toEqual([2,3,4])
-    })
+  it("should return [2,3,4]", () => {
+    const result = callbackFunction([1, 2, 3], function (num) {
+      return num + 1;
+    });
+    expect(result).toEqual([2, 3, 4]);
+  });
 
-    it("should return [.5, 1. 1.5]", ()=>{
-        const result = callbackFunction([1,2,3], function(num) {return num /2});
-        expect(result).toEqual([.5, 1. 1.5])
-    })
+  it("should return [.5, 1. 1.5]", () => {
+    const result = callbackFunction([1, 2, 3], function (num) {
+      return num / 2;
+    });
+    expect(result).toEqual([0.5, 1, 1.5]);
+  });
 
-    it("should return [-1, 0, 1]", ()=>{
-        const result = callbackFunction([1,2,3], function(num) {return num - 2});
-        expect(result).toEqual([-1, 0, 1])
-    })
+  it("should return [-1, 0, 1]", () => {
+    const result = callbackFunction([1, 2, 3], function (num) {
+      return num - 2;
+    });
+    expect(result).toEqual([-1, 0, 1]);
+  });
 });
